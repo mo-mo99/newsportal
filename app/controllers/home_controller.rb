f69dad0bf1072cf7fb5bc5ba15cb069f6ead6575
mamad <mohtashamimohamad99@gmail.com>
2022-07-11 17:44:11 +0300

    added account setting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  before_action :eligable?, only: %i(setting profile)
+
   def index
+  end
+
+  def profile
+  end
+
+  def setting
+  end
+
+  private
+
+  def eligable?
+    redirect_to "/" if current_user.blank?
   end
 end
